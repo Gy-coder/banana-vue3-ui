75dfa7c92d010e8738805cca1cd9df54e02b5e32
Gy <406631892@qq.com>
2021-06-24 18:51:32 +0800

    实现点击切换以及鼠标进入离开事件

--- src/components/SlidesPage.vue
+++ src/components/SlidesPage.vue
@@ -1,26 +1,33 @@
 <template>
-  <Slides v-model:selected="selected" autoPlay>
+  {{ selected }}
+  <Slides v-model:selected="selected" :autoPlay="true ">
     <SlidesItem>
-      <div class="box">0</div>
+      <div class="box">
+        <img src="https://i02piccdn.sogoucdn.com/51d9e47d9c41fdc4" alt="">
+      </div>
     </SlidesItem>
     <SlidesItem>
-      <div class="box">1</div>
+      <div class="box">
+        <img src="https://p0.ssl.img.360kuai.com/bdr/640__/t01c2c26d0437f6caaf.jpg" alt="">
+      </div>
     </SlidesItem>
     <SlidesItem>
-      <div class="box">2</div>
+      <div class="box">
+        <img src="http://5b0988e595225.cdn.sohucs.com/images/20170719/c075af9858d949e6916c5228772842b6.jpeg" alt="">
+      </div>
     </SlidesItem>
   </Slides>
 </template>
 <script>
 import Slides from "../lib/Slides/Slides.vue";
 import SlidesItem from "../lib/Slides/SlidesItem.vue";
-import { ref } from "vue";
+import {ref} from "vue";
 
 export default {
-  components: { SlidesItem, Slides },
+  components: {SlidesItem, Slides},
   setup() {
     const selected = ref(1);
-    return { selected };
+    return {selected};
   },
 };
 </script>
@@ -29,9 +36,16 @@ export default {
 <style lang="scss" scoped>
 .box {
   width: 100%;
-  height: 350px;
+  height: 300px;
   background: pink;
   border: 1px solid black;
   box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  > img{
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
--- src/lib/Slides/Slides.vue
+++ src/lib/Slides/Slides.vue
@@ -1,26 +1,36 @@
 <template>
-  <div class="g-slides">
+  <div class="g-slides" @mouseenter="onMouseEnter" @mouseleave="onMouseMove">
     <div class="g-slides-window">
-      <div class="g-slides-wrapper">
+      <div class="g-slides-window-wrapper">
         <transition-group
-          :name="`slide${isReverse ? '-reverse' : ''}`"
-          mode="out-in"
+            :name="`slide${isReverse ? '-reverse' : ''}`"
+            mode="out-in"
         >
           <component
-            v-for="(child, index) in children"
-            :is="child"
-            :key="index"
-            :index="index"
-            v-show="curIndex === index"
+              v-for="(child, index) in children"
+              :is="child"
+              :key="index"
+              :index="index"
+              v-show="curIndex === index"
           ></component>
         </transition-group>
       </div>
+    </div>
+    <div class="g-slides-dot">
+      <span
+          v-for="(_,index) in children"
+          :key="index"
+          :class="{active: curIndex === index}"
+          @click="onSelect(index)"
+      >
+        {{ index }}
+      </span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { onBeforeUnmount, onMounted, ref } from "vue";
+import {onBeforeUnmount, onMounted, ref} from 'vue';
 
 export default {
   props: {
@@ -34,79 +44,137 @@ export default {
   },
   setup(props, context) {
     const children = context.slots.default();
-    const { length } = children;
+    const {length} = children;
     const curIndex = ref(props.selected);
     const isReverse = ref(false);
     let id;
     const onPre = () => {
       isReverse.value = true;
       curIndex.value = (curIndex.value + length - 1) % length;
-      context.emit("update:selected", curIndex.value);
+      context.emit('update:selected', curIndex.value);
     };
     const onNext = () => {
       isReverse.value = false;
       curIndex.value = (curIndex.value + length + 1) % length;
-      context.emit("update:selected", curIndex.value);
+      context.emit('update:selected', curIndex.value);
     };
-    const playAutomatically = () => (id = setInterval(onNext, 3000));
+    const onSelect = (index) => {
+      isReverse.value = curIndex.value > index;
+      curIndex.value = index;
+      context.emit('update:selected', curIndex.value);
+    };
+    const playAutomatically = () => {
+      id = setInterval(onNext, 3000)
+    };
+    const onMouseEnter = ()=>{
+      window.clearInterval(id)
+      id = undefined
+    }
+    const onMouseMove = ()=>{
+      if(id) return
+      playAutomatically()
+    }
     onMounted(() => {
       if (props.autoPlay) playAutomatically();
     });
     onBeforeUnmount(() => {
       if (props.autoPlay) clearInterval(id);
     });
-    return { children, curIndex, isReverse };
+    return {
+      children,
+      curIndex,
+      isReverse,
+      onSelect,
+      onMouseEnter,
+      onMouseMove,
+      onPre,
+      onNext,
+    };
   },
 };
 </script>
 
 <style lang="scss">
+.g-slides {
+  &-window {
+    overflow: hidden;
+
+    &-wrapper {
+      position: relative;
+    }
+  }
+
+  &-dot {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    > span {
+      display: inline-flex;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background: #ddd;
+      justify-content: center;
+      align-items: center;
+      margin: 0 8px;
+      &:hover{
+        cursor: pointer;
+      }
+      &.active{
+        background: black;
+        color: white;
+        &:hover{
+          cursor: default;
+        }
+      }
+    }
+  }
+}
+</style>
+
+<style lang="scss">
 .slide-enter-active,
 .slide-leave-active {
-  transition: all 1s;
+  transition: all .5s;
 }
+
 .slide-leave-active,
-.slide-reverse-leave-active{
+.slide-reverse-leave-active {
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
-  height: 100% ;
+  height: 100%;
 }
+
 .slide-enter-from {
   transform: translateX(100%);
 }
+
 .slide-enter-to,
 .slide-leave-from {
   transform: translateX(0%);
 }
+
 .slide-leave-to {
   transform: translateX(-100%);
 }
 
 .slide-reverse-enter-active,
 .slide-reverse-leave-active {
-  transition: all 1s;
+  transition: all .5s;
 }
+
 .slide-reverse-enter-from {
   transform: translateX(-100%);
 }
+
 .slide-reverse-enter-to,
 .slide-reverse-leave-from {
   transform: translateX(0%);
 }
+
 .slide-reverse-leave-to {
   transform: translateX(100%);
-}
-
-.g-slides {
-  &-window{
-    min-width: 100px;
-    min-height: 100px;
-    overflow: hidden;
-  }
-  &-wrapper {
-    position: relative;
-  }
 }
 </style>
