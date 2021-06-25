27f52ea2755aadcb3015ad2d7ec6886af9771a35
Gy <406631892@qq.com>
2021-06-25 10:49:12 +0800

    使用ts改写eventbus

---
+++ src/util/eventbus.ts
@@ -0,0 +1,40 @@
+export default class EventHub {
+  private cache: { [K: string]: Array<Function> } = {}; // {name:[fn1,fn2,fn3]}
+  on(name: string, fn: Function) {
+    this.cache[name] = this.cache[name] || [];
+    this.cache[name].push(fn);
+  }
+
+  emit(name: string, data?) {
+    (this.cache[name] || []).forEach((fn) => {
+      fn(data);
+    });
+  }
+
+  off(name: string, fn: Function) {
+    let index = indexOf(this.cache[name], fn);
+    if (index === -1) {
+      return;
+    }
+    this.cache[name].splice(index, 1);
+  }
+}
+
+/**
+ * 辅助函数indexOf
+ * @param arr : Array<any>
+ * @param item : any
+ * @return number
+ */
+
+function indexOf(arr: Array<any>, item: any) {
+  if (arr === undefined) return -1;
+  let index = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
